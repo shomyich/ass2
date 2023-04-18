27bd919e0b305eee3777e667301aaf72c0635033
shomyich <220009@astanait.edu.kz>
2023-04-18 20:30:29 +0600

    implement MyList

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -2,4 +2,58 @@ import java.util.Arrays;
 
 public class MyArrayList<T> implements MyList<T> {
 
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public void add(T item) {
+
+    }
+
+    @Override
+    public void add(T item, int index) {
+
+    }
+
+    @Override
+    public boolean remove(T item) {
+        return false;
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
 }
